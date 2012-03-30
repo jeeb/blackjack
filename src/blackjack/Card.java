22c4dc499e248721c3657e3a800a07b2c78b9902
Jan E <jeebjp@gmail.com>
2012-03-30 14:16:46 +0300

    Put back in stuff that might be useful.

--- src/blackjack/Card.java
+++ src/blackjack/Card.java
@@ -14,7 +14,10 @@ import java.util.*;
 public class Card {
     public enum Suit { CLUBS, DIAMONDS, HEARTS, SPADES }
     public enum Rank { DEUCE, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE,
-                        TEN, JACK, QUEEN, KING, ACE }
+                       TEN, JACK, QUEEN, KING, ACE }
+    
+    private final Suit suit;
+    private final Rank rank;
     
     /*
      * Changing ints to enums
@@ -22,6 +25,11 @@ public class Card {
      * private int rank;
     */
     private boolean isConcealed;
+    
+    private Card(Suit suit, Rank rank) {
+       this.suit = suit;
+       this.rank = rank;
+    }
     
     
 }

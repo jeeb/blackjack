99a846db423870784a7468252507d862e4315c50
Jan E <jeebjp@gmail.com>
2012-03-30 14:15:37 +0300

    Switch certain ints to enums. see http://download.oracle.com/javase/1.5.0/docs/guide/language/enums.html for examples.

--- src/blackjack/Card.java
+++ src/blackjack/Card.java
@@ -10,11 +10,19 @@ package blackjack;
  * @author jeekstro
  */
 public class Card {
-    private int suit;
-    private int rank;
+    private enum suit { CLUBS, DIAMONDS, HEARTS, SPADES }
+    private enum rank { DEUCE, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE,
+                        TEN, JACK, QUEEN, KING, ACE }
+    
+    
+    /*
+     * Changing ints to enums
+     * private int suit;
+     * private int rank;
+    */
     private boolean isConcealed;
     
     public Card() {
-        
+         
     }
 }
